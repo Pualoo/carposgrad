c92023df9663d9aaa7c51cb57a2ade2794f36821
“Paulo <phchirnev@hotmail.com>
2025-09-03 23:35:50 -0300

    feat: create CarItemView composable for displaying car details

---
+++ app/src/main/java/com/example/myapitest/view/dashboard/CarItemView.kt
@@ -0,0 +1,40 @@
+
+package com.example.myapitest.view.dashboard
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.example.myapitest.model.Car
+
+@Composable
+fun CarItemView(car: Car) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(model = car.imageUrl),
+            contentDescription = "Car Image",
+            modifier = Modifier.size(60.dp)
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Column {
+            Text(text = car.name)
+            Text(text = car.year)
+            Text(text = car.license)
+        }
+    }
+}
