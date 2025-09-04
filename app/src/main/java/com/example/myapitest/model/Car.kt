b5d61a813f49fd5b717ba2efb59bca93cdfe0c6c
“Paulo <phchirnev@hotmail.com>
2025-09-04 17:28:09 -0300

    feat: add serialization annotations to Car and Place data classes

--- app/src/main/java/com/example/myapitest/model/Car.kt
+++ app/src/main/java/com/example/myapitest/model/Car.kt
@@ -3,15 +3,15 @@ package com.example.myapitest.model
 import com.google.gson.annotations.SerializedName
 
 data class Place(
-    val lat: Double,
-    val long: Double
+    @SerializedName("lat") val lat: Double,
+    @SerializedName("long") val long: Double
 )
 
 data class Car(
-    val id: String? = null,
-    val imageUrl: String,
-    val name: String,
-    val year: String,
-    val licence: String,
-    val place: Place
+    @SerializedName("id") val id: String,
+    @SerializedName("imageUrl") val imageUrl: String,
+    @SerializedName("year") val year: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("licence") val licence: String,
+    @SerializedName("place") val place: Place
 )
