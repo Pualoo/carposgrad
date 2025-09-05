18f8705d381484869c330af881f695b27c7b49da
“Paulo <phchirnev@hotmail.com>
2025-09-05 18:58:23 -0300

    feat: enhance MapScreen layout with car information display and improved map integration

--- app/src/main/java/com/example/myapitest/view/map/MapScreen.kt
+++ app/src/main/java/com/example/myapitest/view/map/MapScreen.kt
@@ -1,12 +1,21 @@
 package com.example.myapitest.view.map
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,10 +27,17 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import com.example.myapitest.viewmodel.CarViewModel
 import com.google.android.gms.maps.model.LatLng
@@ -59,38 +75,97 @@ fun MapScreen(
             }
         ) { paddingValues ->
             Column(
-                modifier = Modifier.padding(paddingValues)
+                modifier = Modifier
+                    .padding(paddingValues)
+                    .fillMaxSize()
+                    .background(Color.Gray.copy(alpha = 0.1f))
             ) {
                 Image(
                     painter = rememberAsyncImagePainter(model = carModel.imageUrl),
                     contentDescription = "Foto do carro",
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(200.dp),
+                        .height(200.dp)
+                        .clip(RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp)),
                     contentScale = ContentScale.Crop
                 )
 
                 Column(
-                    modifier = Modifier.padding(16.dp)
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Text(text = "Ano: ${carModel.year}")
-                    Text(text = "Placa: ${carModel.licence}")
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceAround,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        InfoText(title = "Ano:", value = carModel.year.toString())
+                        InfoText(title = "Placa:", value = carModel.licence)
+                    }
                 }
 
-                GoogleMap(
-                    modifier = Modifier.weight(1f),
-                    cameraPositionState = rememberCameraPositionState {
-                        position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(
-                            LatLng(carModel.place.lat, carModel.place.long), 15f
-                        )
-                    },
+                Card(
+                    modifier = Modifier
+                        .padding( top = 8.dp)
+                        .weight(1f),
+                    shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
                 ) {
-                    Marker(
-                        state = MarkerState(position = LatLng(carModel.place.lat, carModel.place.long)),
-                        title = carModel.name
-                    )
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        Text(
+                            text = "Localização:",
+                            style = MaterialTheme.typography.headlineSmall,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                        Box(
+                            modifier = Modifier
+                                .weight(1f)
+                                .clip(RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp))
+                        ) {
+                            GoogleMap(
+                                modifier = Modifier.fillMaxSize(),
+                                cameraPositionState = rememberCameraPositionState {
+                                    position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(
+                                        LatLng(carModel.place.lat, carModel.place.long), 15f
+                                    )
+                                }
+                            ) {
+                                Marker(
+                                    state = MarkerState(position = LatLng(carModel.place.lat, carModel.place.long)),
+                                    title = carModel.name
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun InfoText(title: String, value: String) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Text(
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, fontSize = 18.sp)) {
+                    append(title)
+                }
+            },
+            color = Color.Black
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = value,
+            fontSize = 16.sp,
+            color = Color.DarkGray
+        )
     }
 }
